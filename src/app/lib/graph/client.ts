f662cd4adf515ebf4dfa2b45c2f003baabe365cd
emmajane1313 <emma.jane@digitalax.xyz>
2025-04-14 01:18:20 -0400

    graph

--- src/app/lib/graph/client.ts
+++ src/app/lib/graph/client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 export const lucidityClient = new ApolloClient({
   link: new HttpLink({
-    uri: `https://gateway-arbitrum.network.thegraph.com/api/${process.env.NEXT_PUBLIC_GRAPH_KEY}/subgraphs/id/E8AWp1HBjHbEDvJMuCpHFcSfz8vmV7Z8fAjALGob2HNk`,
+    uri: `https://gateway-arbitrum.network.thegraph.com/api/${process.env.NEXT_PUBLIC_GRAPH_KEY}/69Y3tNegVngQu4V89UotXB8Z7fPC3TLsPnCVWWvhQpF5`,
   }),
   cache: new InMemoryCache(),
 });
