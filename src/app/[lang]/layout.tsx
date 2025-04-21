06475fc2044075af132e4e9f68499a1b88f467a5
emmajane1313 <emma.jane@digitalax.xyz>
2025-04-21 14:49:27 -0400

    metadata

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -8,12 +8,41 @@ import LeftBarEntry from "../components/Common/modules/LeftBarEntry";
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.lucidity.agentmeme.xyz"),
   title: "Lucidity",
+  description: "Agentic ComfyStream Search.",
   robots: {
     googleBot: {
       index: true,
       follow: true,
     },
   },
+  twitter: {
+    creator: "@emmajane1313",
+    card: "summary_large_image",
+    title: "Lucidity",
+    description: "Agentic ComfyStream Search.",
+  },
+  openGraph: {
+    title: "Lucidity",
+    description: "Agentic ComfyStream Search.",
+  },
+  creator: "Emma-Jane MacKinnon-Lee",
+  publisher: "Emma-Jane MacKinnon-Lee",
+  keywords: [
+    "Web3",
+    "Web3 Fashion",
+    "Moda Web3",
+    "Open Source",
+    "CC0",
+    "Emma-Jane MacKinnon-Lee",
+    "Open Source LLMs",
+    "DIGITALAX",
+    "F3Manifesto",
+    "www.digitalax.xyz",
+    "www.f3manifesto.xyz",
+    "Women",
+    "Life",
+    "Freedom",
+  ],
 };
 
 export default function RootLayout({
